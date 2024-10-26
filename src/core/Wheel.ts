332fba5eb7637fcb7d9b1bc9d37d664814698950
Rogeno <rogicoy@gmail.com>
2024-10-26 22:17:13 +0800

    Renamed the CoreModule to Wheel, the key highlight component

---
+++ src/core/Wheel.ts
@@ -0,0 +1,40 @@
+import { Segment } from './interfaces';
+
+class Wheel<X, Y> {
+  private head: Segment<X, Y>;
+  private tail: Segment<X, Y>;
+  private live: Segment<X, Y>;
+
+  public constructor(seg: Segment<X, Y>) {
+    this.head = seg;
+    this.tail = this.head;
+    this.live = this.head;
+  }
+
+  public addSegment(seg: Segment<X, Y>): void {
+    seg.setNext(this.head);
+    this.tail.setNext(seg);
+    this.tail = seg;
+  }
+
+  public reset(): void {
+    this.live = this.head;
+  }
+
+  public clear(): void {
+    this.live = this.head;
+    this.tail = this.head;
+  }
+
+  public fulfill(arg: X): Y {
+    const next = this.live.getNext();
+
+    if (next?.inScope(arg)) {
+      this.live = next;
+    }
+
+    return this.live.fulfill(arg);
+  }
+}
+
+export default Wheel;
